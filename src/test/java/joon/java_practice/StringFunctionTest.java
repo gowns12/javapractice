2200300a17a135afed97764e2905dc7ed89e8d5d
wjdgowns <wjdgowns4414@gmail.com>
2024-11-25 18:21:06 +0900

    11-25 리스트함수 연습

--- src/test/java/joon/java_practice/StringFunctionTest.java
+++ src/test/java/joon/java_practice/StringFunctionTest.java
@@ -3,8 +3,8 @@ package joon.java_practice;
 import org.assertj.core.api.Assertions;
 import org.junit.jupiter.api.Test;
 
-import java.util.ArrayList;
-import java.util.List;
+import java.util.*;
+import java.util.stream.Stream;
 
 import static org.assertj.core.api.Assertions.assertThat;
 import static org.assertj.core.api.InstanceOfAssertFactories.FLOAT_ARRAY;
@@ -18,6 +18,7 @@ public class StringFunctionTest {
         scores.add(70);
         scores.add(100);
         scores.add(90);
+        scores.add(50);
 
         int sum = 0;
         for (Integer score : scores) {
@@ -35,6 +36,13 @@ public class StringFunctionTest {
         System.out.println("평균값보다 낮은 점수의 리스트: " + avgLow(scores));
 
         System.out.println("평균값보다 높은 점수의 리스트: " + avgHigh(scores));
+
+        Collections.sort(scores);
+
+        System.out.println("오름차순 정렬 : " + scores);
+
+        System.out.println("중앙값은 " + centerValue(scores));
+
     }
 
     int maxList(ArrayList<Integer> List) {
@@ -86,4 +94,70 @@ public class StringFunctionTest {
         }
         return answer;
     }
+
+    ArrayList<Integer> centerValue(ArrayList<Integer> scores) {
+        ArrayList<Integer> answer = new ArrayList<>();
+        if (scores.size() % 2 == 0) {
+            answer.add(scores.get(scores.size() / 2 - 1));
+            answer.add(scores.get(scores.size() / 2));
+        } else {
+            answer.add(scores.get(scores.size() / 2));
+        }
+        return answer;
+    }
+
+    @Test
+    void test() {
+        ArrayList<String> 과일 = new ArrayList<>();
+        과일.add("사과");
+        과일.add("바나나");
+
+        copyTest(과일);
+
+        System.out.println(과일);
+
+    }
+
+    void copyTest(ArrayList<String> list) {
+        list.add("추가");
+    }
+
+    @Test
+    void listFunction() {
+        assertThat(getSum(List.of(1, 2, 3, 4))).isEqualTo(10);
+        assertThat(getSum(List.of(7))).isEqualTo(7);
+        assertThat(getSum(List.of())).isEqualTo(0); // 빈 리스트가 주어질 경우 0을 return
+
+        assertThat(getAverage(List.of(1, 2, 3, 4))).isEqualTo(2.5);
+        assertThat(getAverage(List.of(7))).isEqualTo(7.0);
+        assertThat(getAverage(List.of())).isEqualTo(0.0); // 빈 리스트가 주어질 경우 0 return
+
+        assertThat(calculateRanks(List.of(80, 90, 70, 85))).isEqualTo(List.of(3, 1, 4, 2));
+        assertThat(calculateRanks(List.of(100, 100, 80))).isEqualTo(List.of(1, 1, 3));
+        assertThat(calculateRanks(List.of())).isEqualTo(List.of());
+    }
+
+    int getSum(List<Integer> list) {
+        int sum = 0;
+        for (Integer i : list) {
+            sum += i;
+        }
+        return sum;
+    }
+
+    double getAverage(List<Integer> list) {
+        int sum = getSum(list);
+        return list.isEmpty() ? 0 : (double) sum / (double) list.size();
+    }
+
+    List<Integer> calculateRanks(List<Integer> scores){
+        List<Integer> sortedScores = scores.stream().sorted(Comparator.reverseOrder()).toList();
+        new ArrayList<Integer> answer = scores;
+        for (int i = 0; i < sortedScores.size(); i++) {
+            answer.replaceAll(sortedScores.get(0),1);
+        }
+
+        }
+    }
+
 }
